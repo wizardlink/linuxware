5dff38972e4ca75d72708d616ea04d66a4bd76ae
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2023-11-29 18:01:35 -0300

    fix: use wezterm's latest commit as july version is broken in hyprland

--- programs/wezterm/config.nix
+++ programs/wezterm/config.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.wezterm = {
     enable = true;
     extraConfig = builtins.readFile ./wezterm.lua;
+    package = (pkgs.callPackage ./package.nix {});
   };
 }
