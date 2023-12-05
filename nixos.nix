8b1103fb1536917139a1d60085cc83ab5edfaa8f
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2023-12-04 23:28:48 -0300

    fix(nixos): update i18n

--- nixos.nix
+++ nixos.nix
@@ -124,15 +124,15 @@
     defaultLocale = "en_US.UTF-8";
 
     extraLocaleSettings = {
-      LC_ADDRESS = "pt_BR.UTF-8";
-      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
       LC_MEASUREMENT = "pt_BR.UTF-8";
-      LC_MONETARY = "pt_BR.UTF-8";
-      LC_NAME = "pt_BR.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
       LC_NUMERIC = "pt_BR.UTF-8";
       LC_PAPER = "pt_BR.UTF-8";
       LC_TELEPHONE = "pt_BR.UTF-8";
-      LC_TIME = "pt_BR.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
     };
   };
 
