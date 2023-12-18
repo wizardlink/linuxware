c1e45d48fd9a35c27bbb42fbfa75827d259baf6b
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2023-12-18 19:38:06 -0300

    chore: use fsync for guild wars 2

--- scripts/games/launch_gw2.sh
+++ scripts/games/launch_gw2.sh
@@ -5,10 +5,11 @@ export MANGOHUD=1
 export AMD_VULKAN_ICD="RADV"
 export RADV_PERFTEST="sam"
 
-export WINEESYNC=1
+export WINEFSYNC=1
 export WINEPREFIX=/home/wizardlink/Games/guild_wars_2/
 export WINEARCH="win64"
 
-export DXVK_ASYNC=1
+export DXVKV_ASYNC=1
+export DXVK_CONFIG_FILE=/home/wizardlink/.config/dxvk.conf
 
 gamemoderun wine64 /home/wizardlink/Games/guild_wars_2/drive_c/Program\ Files/Guild\ Wars\ 2/Gw2-64.exe -autologin
