5dec1431674f3272366c413af86d37ecb7e2b2d6
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2023-12-30 12:50:26 -0300

    feat: update scripts

--- scripts/steam/launch.sh
+++ scripts/steam/launch.sh
@@ -3,10 +3,9 @@ export MANGOHUD=1
 
 # https://docs.mesa3d.org/envvars.html
 export AMD_VULKAN_ICD="RADV"
-export RADV_PERFTEST="sam"
-
-export PROTON_NO_FSYNC=1
 
 export DXVK_ASYNC=1
+
+export ENABLE_VKBASALT=1
 
 gamemoderun "$@"
