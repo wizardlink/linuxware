596dd0a7e662d7872c60fe755d9f37a514f96f4b
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2024-04-26 07:50:03 -0300

    services(archisteamfarm): enable my user bot

--- services/archi.nix
+++ services/archi.nix
@@ -5,15 +5,15 @@
     enable = true;
     web-ui.enable = true;
 
-    # bots.wizardlink = {
-    #   enabled = true;
-    #   passwordFile = /var/lib/asf/bot_info/wizardlink.password;
-    #   username = builtins.readFile /var/lib/asf/bot_info/wizardlink.username;
-    #
-    #   settings = {
-    #     CustomGamePlayedWhileFarming = "In the fields";
-    #     CustomGamePlayedWhileIdle = "Out from the fields";
-    #   };
-    # };
+    bots.wizardlink = {
+      enabled = true;
+      passwordFile = /var/lib/archisteamfarm/bots/wizardlink.password;
+      username = "master1891891";
+
+      settings = {
+        CustomGamePlayedWhileFarming = "In the fields";
+        CustomGamePlayedWhileIdle = "Out from the fields";
+      };
+    };
   };
 }
