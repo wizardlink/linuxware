17a0e39283c34f5dd33ce2bd1665e84a72568eae
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2024-05-20 11:37:07 -0300

    nixos(jellyfin): remove intro-skipper patch as it doesn't works in 10.9.x

--- services/jellyfin.nix
+++ services/jellyfin.nix
@@ -5,17 +5,5 @@
     enable = true;
     openFirewall = true;
     user = "wizardlink";
-
-    package = pkgs.jellyfin.override {
-      jellyfin-web = pkgs.jellyfin-web.overrideAttrs (oldAttrs: {
-        patches = [
-          (pkgs.fetchpatch {
-            url =
-              "https://github.com/jellyfin/jellyfin-web/compare/v${oldAttrs.version}...ConfusedPolarBear:jellyfin-web:intros.diff";
-            hash = "sha256-qm4N4wMUFc4I53oQJUK1Six0cahVYz3J+FgO2vvSvXM=";
-          })
-        ];
-      });
-    };
   };
 }
