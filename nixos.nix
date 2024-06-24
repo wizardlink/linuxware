a61557e3de6a69cd1e4eb5869144575e393ab32f
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2024-06-24 03:22:26 -0300

    nixos: hardware.opengl -> hardware.graphics

--- nixos.nix
+++ nixos.nix
@@ -235,10 +235,9 @@
   };
 
   # Enable OpenGL.
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
 
     extraPackages = with pkgs; [
       rocm-opencl-icd # OpenGL hwa
@@ -298,6 +297,8 @@
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
+
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
 
   # Enable and configure gamemode.
