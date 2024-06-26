c9d8d353ba20c37c65c40d1f6345499af0eef0e4
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2024-06-26 02:11:25 -0300

    hm: move wezterm lua config to the nix file

--- programs/wezterm/default.nix
+++ programs/wezterm/default.nix
@@ -3,6 +3,25 @@
 {
   programs.wezterm = {
     enable = true;
-    extraConfig = builtins.readFile ./wezterm.lua;
+    extraConfig = # lua
+      ''
+        local wezterm = require("wezterm")
+
+        return {
+        	color_scheme = "Catppuccin Frappe",
+        	enable_wayland = false, -- Unfortunately broken on Hyprland, AGAIN
+        	font = wezterm.font("FantasqueSansM Nerd Font"),
+        	font_size = 13,
+        	hide_tab_bar_if_only_one_tab = true,
+        	hide_mouse_cursor_when_typing = false,
+        	window_background_opacity = 0.88,
+        	window_padding = {
+        		left = 18,
+        		right = 18,
+        		top = 18,
+        		bottom = 18,
+        	},
+        }
+      '';
   };
 }
