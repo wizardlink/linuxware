3e51302a8db2aea2e0eeddc9456405a5d3dae965
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2024-07-16 00:47:10 -0300

    obs-studio: remove unused plugins

--- programs/obs-studio.nix
+++ programs/obs-studio.nix
@@ -5,11 +5,7 @@
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
       input-overlay
-      # Currently broken due to onnxruntime failing to build.
-      # obs-backgroundremoval
-      obs-pipewire-audio-capture
-      obs-vaapi
-      obs-vkcapture
+      obs-backgroundremoval
     ];
   };
 }
