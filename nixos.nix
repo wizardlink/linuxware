5434de614dfc39e975173d47cab8a1850db769cd
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2024-08-10 17:22:05 -0300

    hyprland: specify the xdg-desktop-portal package from git flake

--- nixos.nix
+++ nixos.nix
@@ -262,6 +262,7 @@ in
     enable = true;
 
     package = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   # Enable XDG Desktop Portals.
