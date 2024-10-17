e91a69487e576099b774e4d626a2650b1379b112
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2024-10-17 20:33:02 -0300

    chore(neovim): remove discord presence plugin

--- modules/home-manager/programs/neovim/lua/plugins/user.lua
+++ modules/home-manager/programs/neovim/lua/plugins/user.lua
@@ -3,17 +3,6 @@
 
 ---@type LazySpec
 return {
-
-  -- Discord presence
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function()
-      require("lsp_signature").setup()
-    end,
-  },
-
   -- Customize alpha options
   {
     "goolord/alpha-nvim",
