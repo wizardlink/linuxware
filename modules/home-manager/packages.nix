13a96aaea8092e796da87256fdbef5ba08b0a3c2
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2024-11-14 17:39:21 -0300

    fix: move fhs to be specific to home-manager per user

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -79,31 +79,5 @@
 
     # Mail client
     thunderbird
-
-    # Create an FHS environment using the command `fhs`, enabling the execution of non-NixOS packages in NixOS!
-    (
-      let
-        base = appimageTools.defaultFhsEnvArgs;
-      in
-      buildFHSUserEnv (
-        base
-        // {
-          name = "fhs";
-          targetPkgs =
-            pkgs:
-            (
-              # pkgs.buildFHSUserEnv provides only a minimal FHS environment,
-              # lacking many basic packages needed by most software.
-              # Therefore, we need to add them manually.
-              #
-              # pkgs.appimageTools provides basic packages required by most software.
-              (base.targetPkgs pkgs) ++ (with pkgs; [ nodejs ])
-            );
-          profile = "export FHS=1";
-          runScript = "bash";
-          extraOutputsToInstall = [ "dev" ];
-        }
-      )
-    )
   ];
 }
--- specific/desktop/home-manager.nix
+++ specific/desktop/home-manager.nix
@@ -79,6 +79,36 @@
 
   home.packages = with pkgs; [
     pcsx2
+
+    # Create an FHS environment using the command `fhs`, enabling the execution of non-NixOS packages in NixOS!
+    (
+      let
+        base = appimageTools.defaultFhsEnvArgs;
+      in
+      buildFHSUserEnv (
+        base
+        // {
+          name = "fhs";
+          targetPkgs =
+            pkgs:
+            (
+              # pkgs.buildFHSUserEnv provides only a minimal FHS environment,
+              # lacking many basic packages needed by most software.
+              # Therefore, we need to add them manually.
+              #
+              # pkgs.appimageTools provides basic packages required by most software.
+              (base.targetPkgs pkgs)
+              ++ (with pkgs; [
+                nodejs
+                dotnet-sdk_8
+              ])
+            );
+          profile = "export FHS=1";
+          runScript = "bash";
+          extraOutputsToInstall = [ "dev" ];
+        }
+      )
+    )
   ];
 
   #
