c770d8e0382beb426e48ad0e2cb13e820f16802e
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2024-11-30 13:36:52 -0300

    fix(gamemode): properly configure gpu settings

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -26,6 +26,13 @@
   programs.gamemode = {
     enable = true;
     enableRenice = true;
+    settings = {
+      gpu = {
+        apply_gpu_optimisations = "accept-responsibility";
+        gpu_device = 1;
+        amd_performance_level = "auto";
+      };
+    };
   };
 
   # Enable KDEConnect
