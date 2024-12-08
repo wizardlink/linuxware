c82f183021b923267d8f2369813c51aecb72cc5e
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2024-12-08 19:54:27 -0300

    fix(services/terraria): create a named socket for tmux

--- specific/desktop/services/terraria.nix
+++ specific/desktop/services/terraria.nix
@@ -7,7 +7,7 @@
     Unit.Description = "Terraria TMUX Server";
 
     Service = {
-      ExecStart = "${pkgs.tmux}/bin/tmux -S /run/user/1000/tmux-1000/terraria new -s Terraria -d /etc/profiles/per-user/wizardlink/bin/fhs -c 'dotnet ./tModLoader.dll -server -config serverconfig.txt'";
+      ExecStart = "${pkgs.tmux}/bin/tmux -L terraria new -s Terraria -d /etc/profiles/per-user/wizardlink/bin/fhs -c 'dotnet ./tModLoader.dll -server -config serverconfig.txt'";
       ExecStop = "${pkgs.tmux}/bin/tmux kill-session -t Terraria";
       Restart = "on-failure";
       Type = "forking";
