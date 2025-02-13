c0499e58ed481971ef0ed36a638d2f21ae1999d6
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2025-02-12 22:06:18 -0300

    fix(neovim/queries/c_sharp): add offset to endregion and detect statements and comments

--- modules/home-manager/programs/neovim/queries/c_sharp/folds.scm
+++ modules/home-manager/programs/neovim/queries/c_sharp/folds.scm
@@ -5,10 +5,12 @@
   (preproc_region) @region_begin
   .
   [
+    (comment)
     (declaration)
+    (statement)
     (type_declaration)
   ]*
   .
-  (preproc_endregion) @region_end
+  (preproc_endregion) @region_end (#offset! @region_end 0 0 -1 0)
   (#make-range! "fold" @region_begin @region_end)
 )
