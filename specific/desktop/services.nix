9a4e5c40ef444e8fec7162270c6bf19eb18029b8
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2025-02-21 00:01:10 -0300

    chore(desktop): move postgresql service to it's own file

--- specific/desktop/services.nix
+++ specific/desktop/services.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   imports = [
@@ -6,15 +6,6 @@
     ./services/caddy.nix
     ./services/forgejo.nix
     ./services/jellyfin.nix
+    ./services/postgresql.nix
   ];
-
-  services.postgresql = {
-    enable = true;
-
-    initialScript = pkgs.writeText "backend-initScript" ''
-      CREATE ROLE wizardlink WITH LOGIN SUPERUSER PASSWORD 'wizardlink' CREATEDB CREATEROLE REPLICATION;
-      CREATE DATABASE wizardlink;
-      GRANT ALL PRIVILEGES ON DATABASE wizardlink TO wizardlink;
-    '';
-  };
 }
