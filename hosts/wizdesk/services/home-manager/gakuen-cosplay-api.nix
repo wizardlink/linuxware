f7c1959973a215f8e545da5d004de59d75958dfb
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2025-03-16 00:04:30 -0300

    chore: update gakuen-cosplay input and gakuen-cosplay-api service

--- hosts/wizdesk/services/home-manager/gakuen-cosplay-api.nix
+++ hosts/wizdesk/services/home-manager/gakuen-cosplay-api.nix
@@ -7,7 +7,7 @@
     Unit.Description = "Gakuen Cosplay API";
 
     Service = {
-      ExecStart = "${gakuen-cosplay.packages.${pkgs.system}.backend}/bin/cosplayer_submission";
+      ExecStart = "${gakuen-cosplay.packages.${pkgs.system}.backend}/bin/animegakuen-cosplay-backend";
       Restart = "on-failure";
       Type = "simple";
       WorkingDirectory = "/srv/gakuen_api";
