79cfc08e30c3dbbb67a32e3e74b16cab9738ef74
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2025-04-23 16:42:52 -0300

    feat(neovim): add icon for razor files

---
+++ modules/neovim/lua/plugins/mini-icons.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  "echasnovski/mini.icons",
+  lazy = true,
+  opts = {
+    extension = {
+      cshtml = {
+        glyph = "󱦗",
+        hl = "MiniIconsPurple",
+      },
+    },
+  },
+}
