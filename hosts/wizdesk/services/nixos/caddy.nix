2b551e1887fa67924228a359eb3a013f136d2e1d
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2025-04-23 16:37:37 -0300

    chore(wizdesk/caddy): add x-robots-tag header to certain hosts

--- hosts/wizdesk/services/nixos/caddy.nix
+++ hosts/wizdesk/services/nixos/caddy.nix
@@ -11,16 +11,19 @@
 
     virtualHosts."jellyfin.thewizard.link".extraConfig = ''
       encode gzip
+      header X-Robots-Tag "none"
       reverse_proxy 127.0.0.1:8096 {
         flush_interval -1
       }
     '';
 
     virtualHosts."jellyseerr.thewizard.link".extraConfig = ''
+      header X-Robots-Tag "none"
       reverse_proxy http://127.0.0.1:5055
     '';
 
     virtualHosts."foundry.thewizard.link".extraConfig = ''
+      header X-Robots-Tag "none"
       reverse_proxy 127.0.0.1:30000 {
           flush_interval -1
         }
@@ -32,18 +35,22 @@
 
     virtualHosts."files.thewizard.link".extraConfig = ''
       root * /srv/files
+      header X-Robots-Tag "none"
       file_server
     '';
 
     virtualHosts."torrent.thewizard.link".extraConfig = ''
+      header X-Robots-Tag "none"
       reverse_proxy 127.0.0.1:8144
     '';
 
     virtualHosts."shoko.thewizard.link".extraConfig = ''
+      header X-Robots-Tag "none"
       reverse_proxy 127.0.0.1:8111
     '';
 
     virtualHosts."nixbin.thewizard.link".extraConfig = ''
+      header X-Robots-Tag "none"
       reverse_proxy 127.0.0.1:7373
     '';
   };
