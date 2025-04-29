8aa5a877a1eca8250d0e190ac5f063e7830ddb87
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2025-04-29 17:12:52 -0300

    chore(neovim): remove telescope spec from catppuccin

--- modules/neovim/lua/plugins/user.lua
+++ modules/neovim/lua/plugins/user.lua
@@ -71,16 +71,6 @@ return {
           })
         end,
       },
-      {
-        "nvim-telescope/telescope.nvim",
-        optional = true,
-        opts = {
-          highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-          },
-        },
-      },
     },
   },
 
