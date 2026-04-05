6cb471ea808da3b05a02f874333070146abb81a1
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2026-04-05 13:06:08 -0300

    chore(shared/nixos): configure amd gpu for gaming performance

--- shared/nixos/hardware.nix
+++ shared/nixos/hardware.nix
@@ -6,6 +6,14 @@
     "amdgpu"
   ];
 
+  # Tune power management
+  boot.extraModprobeConfig = ''
+    options amdgpu runpm=0 aspm=0 bapm=0
+  '';
+  services.udev.extraRules = ''
+    SUBSYSTEM=="drm", DRIVERS=="amdgpu", ATTR{device/power_dpm_force_performance_level}="high"
+  '';
+
   # Enable Bluetooth
   hardware.bluetooth = {
     enable = true;
