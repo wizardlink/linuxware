d51c97479f38ef9989cb683bf8e9ab4abc39bd6f
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2026-05-17 17:37:17 -0300

    chore(hosts): update locales

--- hosts/wizdesk/nixos.nix
+++ hosts/wizdesk/nixos.nix
@@ -111,7 +111,7 @@
   # Select internationalisation properties.
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    supportedLocales = [
+    extraLocales = [
       "C.UTF-8/UTF-8"
       "en_GB.UTF-8/UTF-8"
       "en_US.UTF-8/UTF-8"
@@ -121,16 +121,16 @@
 
     extraLocaleSettings = {
       LANGUAGE = "en_US.UTF-8";
-      LC_ADDRESS = "en_US.UTF-8";
+      LC_ADDRESS = "pt_BR.UTF-8";
       LC_ALL = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
       LC_MEASUREMENT = "pt_BR.UTF-8";
       LC_MONETARY = "en_US.UTF-8";
       LC_NAME = "en_US.UTF-8";
       LC_NUMERIC = "pt_BR.UTF-8";
       LC_PAPER = "pt_BR.UTF-8";
       LC_TELEPHONE = "pt_BR.UTF-8";
-      LC_TIME = "en_GB.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
     };
   };
 
